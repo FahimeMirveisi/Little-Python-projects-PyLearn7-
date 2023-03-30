fc749d3930d564db992cdf88fb8da8af89c7643b
fahimecodes <fahime.mir75@gmail.com>
2023-03-30 04:40:14 +0430

    correction of sin-cos-tan-cot input

--- Assignment1/calculator.py
+++ Assignment1/calculator.py
@@ -1,58 +1,70 @@
 import math
-print("*** Wellcome to my calculator ***")
+print("*** Wellcome to calculator ***")
 
-print("+ : sum")
-print("- : sub")
-print("* : mul")
-print("/ : div")
-print("sqrt")
-print("sin")
-print("cos")
-print("tan")
-print("cot")
-print("factorial")
-
-print("Please enter your choice: ")
-op = input()
-
-if op == '+' or op == '-' or op == '*' or op == '/':
-    input_num1 = float(input("Enter number 1: "))
-    input_num2 = float(input("Enter number 2: "))
-else:
-    input_num = float(input("Enter number: "))
+while 1:
+    print("+ : sum")
+    print("- : sub")
+    print("* : multiplication")
+    print("/ : division")
+    print("sqrt")
+    print("factorial")
+    print("sin")
+    print("cos")
+    print("tan")
+    print("cot")
 
 
-if op == '+':
-    result = input_num1 + input_num2
+    print("Please enter your choice: ")
+    op = input()
 
-elif op == '-':
-    result = input_num1 - input_num2
-
-elif op == '*':
-    result = input_num1 * input_num2
-
-elif op == '/':
-    if input_num2 == 0:
-        result = "Cannot divide by zero"
+    if op == '+' or op == '-' or op == '*' or op == '/':
+        input_num1 = float(input("Enter number 1: "))
+        input_num2 = float(input("Enter number 2: "))
     else:
-        result = input_num1 / input_num2
+        input_num = float(input("Enter number: "))
 
-elif op == 'sqrt':
-    result = math.sqrt(input_num)
 
-elif op == 'sin':
-    result = math.sin(input_num)
+    if op == '+':
+        result = input_num1 + input_num2
 
-elif op == 'cos':
-    result = math.cos(input_num)
+    elif op == '-':
+        result = input_num1 - input_num2
 
-elif op == 'tan':
-    result = math.tan(input_num)
+    elif op == '*':
+        result = input_num1 * input_num2
 
-elif op == 'cot':
-    result = math.cot(input_num)
+    elif op == '/':
+        if input_num2 == 0:
+            result = "Cannot divide by zero"
+        else:
+            result = input_num1 / input_num2
 
-elif op == 'factorial':
-    result = math.factorial(input_num)
+    elif op == 'sqrt':
+        result = math.sqrt(input_num)
 
-print(result)
+    elif op == 'sin':
+        radian_input = math.radians(input_num)
+        result = math.sin(radian_input)
+
+    elif op == 'cos':
+        radian_input = math.radians(input_num)
+        result = math.cos(radian_input)
+
+    elif op == 'tan':
+        radian_input = math.radians(input_num)
+        result = math.tan(radian_input)
+
+    elif op == 'cot':
+        radian_input = math.radians(input_num)
+        result = 1 / math.tan(radian_input)
+
+    elif op == 'factorial':
+        result = math.factorial(input_num)
+
+    print(result)
+
+    end = input('Do you want to continue? (yes/no)')
+    if end == 'no':
+        print('Goodbye--- Wish to see you again...')
+        break
+
