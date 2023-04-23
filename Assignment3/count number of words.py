61dabc6c46a3f5d3131206ce76c25b1fa324cf76
fahimecodes <fahime.mir75@gmail.com>
2023-04-23 17:03:47 +0430

    program to count number of sentence wors

---
+++ Assignment3/count number of words.py
@@ -0,0 +1,8 @@
+
+input_sentence = input("Enter your sentence: ")
+
+
+split_list = input_sentence.split()
+
+number_of_words = len(split_list)
+print("Number of sentence words is: ", number_of_words)
