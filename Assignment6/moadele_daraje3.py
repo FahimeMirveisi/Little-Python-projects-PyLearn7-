caaaa0526526713cff1c4e0f0e6dc2d7cb39c693
fahimecodes <fahime.mir75@gmail.com>
2023-05-30 20:14:42 +0330

    start cubic_equation programming

---
+++ Assignment6/moadele_daraje3.py
@@ -0,0 +1,8 @@
+import math
+
+print("Hi welcome\nformat of Grade 3 equation is like this: \nax3 + bx2 + cx + d = 0")
+
+def cubic_equation(a, b, c, d):
+    delta = 18*a*b*c - 4*(b**3)*d + (b**2)*(c**2) - 4*a*(c**3) - 27*(a**2)*(d**2)
+
+
